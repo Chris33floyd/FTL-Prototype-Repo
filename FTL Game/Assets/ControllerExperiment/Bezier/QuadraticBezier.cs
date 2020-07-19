4cde943caaed5fe8fd95dbde37d3a5371e37e9c6
Chris33floyd <34195708+Chris33floyd@users.noreply.github.com>
2020-07-19 18:59:28 +0100

    Bezier Files

---
+++ FTL Game/Assets/ControllerExperiment/Bezier/QuadraticBezier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ControllerExperiment
+{
+    public class QuadraticBezier : BezierBase
+    {
+		public override void GetBezier(out Vector3 pos, List<GameObject> Checkpoints, float time)
+		{
+			if (Checkpoints.Count > 3)
+			{
+				for (int i = 3; i < Checkpoints.Count; i++)
+				{
+					Checkpoints.RemoveAt(i);
+				}
+			}
+
+			QuadraticBezierEquation.GetCurve(out pos,
+				Checkpoints[0].transform.position,
+				Checkpoints[1].transform.position,
+				Checkpoints[2].transform.position,
+				time);
+		}
+
+		private void Update()
+		{
+			GetBezier(out myPosition, Checkpoints, time);
+			this.transform.position = myPosition;
+		}
+	}
+}
