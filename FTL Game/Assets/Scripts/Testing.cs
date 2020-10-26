d2e64619cddff194ef909d84d841569339e8c77b
Chris33floyd <34195708+Chris33floyd@users.noreply.github.com>
2020-10-26 14:18:01 +0000

    Project Rethink and Basic Grid

---
+++ FTL Game/Assets/Scripts/Testing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+public class Testing : MonoBehaviour
+{
+	private Grid grid;
+
+    void Start()
+    {
+		grid = new Grid(20, 10, 10f, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Left Mouse Button
+		//if (Input.GetMouseButtonDown(0))
+		//{
+		//	grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+		//}
+		////Right Mouse button
+		//if (Input.GetMouseButtonDown(1))
+		//{
+		//	Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+		//}
+    }
+}
